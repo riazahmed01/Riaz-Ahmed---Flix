c157e50403deb2b8b3109d28cc928de1c377e0f0
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2021-02-18 18:21:54 -0500

    Initial Commit

---
+++ Riaz Ahmed - Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Riaz Ahmed - Flix
+//
+//  Created by RIAZ on 2/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
